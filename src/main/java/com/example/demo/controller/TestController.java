dfa891bf2bc19b83fd9596b0bd231857f8a449ea
wllpeter <13162579298@163.com>
2018-05-14 12:42:06 +0800

    dubbo

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.service.TestService;
+import com.example.demo.service.PersonService;
+import com.example.demo.service.TestService;
+import com.example.demo.util.Response;
 
 
 
@@ -16,9 +18,20 @@ public class TestController {
 
 	@Resource
     private TestService testService;
+    @Resource
+    private PersonService personService;
+    
     @RequestMapping(value = "/test" ,method = RequestMethod.GET)
     @ResponseBody
     public String test(){
         return testService.sayHello("Chenqimiao");
     }
+    
+    @RequestMapping(value = "/person" ,method = RequestMethod.GET)
+    @ResponseBody
+    public Response person(){
+     
+        return Response.success(personService.findAllUser());
+    }
+
 }
